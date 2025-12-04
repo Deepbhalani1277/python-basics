0a27afcc4f640f57fb5d24b8ecf16fff49cd42d8
Deepbhalani1277 <patel.deep1277@gmail.com>
2025-12-04 22:42:30 +0530

    update

--- exercises/secretlang.py
+++ exercises/secretlang.py
@@ -1,6 +1,26 @@
-message = input("enter message:")
- 
-print(len(message))
-
-if(len(message) >= 3):
-    
+st = input("enter the message:")
+words = st.split()
+coding = False
+if(coding):
+    nwords = []
+    for word in words:
+        # print(word)
+        if(len(word)>=3):
+            r1="def"
+            r2="res"
+            stnew = r1 + word[1:] + word[0] + r2
+            nwords.append(stnew)
+        else:
+            nwords.append(word[::-1])
+    print(" ".join(nwords))
+else:
+    nwords = []
+    for word in words:
+        # print(word)
+        if(len(word)>=3):
+            stnew = word[3:-3]
+            stnew = stnew[-1] + stnew[:-1]
+            nwords.append(stnew)
+        else:
+            nwords.append(word[::-1])
+    print(" ".join(nwords))
