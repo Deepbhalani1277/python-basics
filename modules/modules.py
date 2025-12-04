49e05d4c273f6ba1c85588bd40548065a723ff2a
Deepbhalani1277 <patel.deep1277@gmail.com>
2025-12-04 22:17:03 +0530

    updated

--- modules/modules.py
+++ modules/modules.py
@@ -3,8 +3,11 @@ import os
 if (not os.path.exists("modules/data")):
     os.mkdir("modules/data")
 
-for i in range(1,100):
+# for i in range(1,100):
     # os.mkdir(f"modules/data/data{i}")
     # os.rmdir(f"Data{i}")
     # os.rename(f"modules/data/data{i}", f"modules/data/day{i}")
-    
+    # os.rmdir(f"modules/data/day{i}")
+folders = os.listdir("modules/data")
+for i in folders:
+    print(os.listdir(f"modules/data/{i}"))
